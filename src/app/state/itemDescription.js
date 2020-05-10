8869aa019158fd8b344f2c220c99f3c38ccb90b7
Vipul Chohan <vipul.chohan@nagarro.com>
2020-05-10 22:31:53 +0530

    Added product description

--- src/app/state/itemDescription.js
+++ src/app/state/itemDescription.js
@@ -1,5 +1,20 @@
 const ProductItemDesciption ={
-    AlmondsDesc : 'Rich in taste and nutrition. Sprinkle over a variety of recipes like desserts, sundaes, biscuits, cakes, etc.Offers an extra crunch in every bite.'
+    AlmondsDesc : 'Rich in taste and nutrition. Sprinkle over a variety of recipes like desserts, sundaes, biscuits, cakes, etc.Offers an extra crunch in every bite.',
+    AppleDesc :'Apple is a good source of vitamin C and dietary fibre, which helps to keep the immune system and digestive system functioning well. It is also rich in anti-oxidant.',
+    BreadDesc :'Bread is made from whole wheat flour and not enriched flour. Soft and spongy slices that are perfect for all types of sandwiches! We hope you enjoy eating it as much as we enjoyed baking it.',
+    CucumberDesc :'Cucumber is a variety of seedless cucumber that is longer and slimmer than other varieties and have a higher water content. They do not have a layer of wax on them, and the skin is tender when ripe. ',
+    FlourDesc :'Apart from whole grain, this product contains important vitamins, minerals and calcium, all of which are good for a healthy body.',
+    GrapesDesc :'Juicy and snappy, with a beautiful balance of sweet and tart flavour, these grape bunches have a lingering taste. ',
+    GuavaDesc :'Savour the green guavas along with hard, pale yellow edible seeds. The off-white flesh is crunchy and mildly sweet with very good fragrance. ',
+    LichiDesc :'Lychee contains good amount of antioxidant Vitamin C, Vitamin B-complex and phytonutrient flavonoids.',
+    MangoesDesc :'Mangos are known to contain more than 20 different vitamins and minerals, which make them a superfood. Laden with the goodness of Vitamin C and A, mangoes also have a high fibre content.',
+    MilkDesc :'Milk builds healthy bones and teeth,  maintains bone mass and serves as a source of energy.',
+    MushroomsDesc :'Mushrooms boost our immune system. They have anticancer benefits.',
+    OnionDesc :'Onion is a vegetable which is almost like a staple in Indian food. This is also known to be one of the essential ingredients of raw salads. They come in different colours like white, red or yellow and are quite in demand in cold salads and hot soups.',
+    PomegranateDesc :'Pomegranates are rich in anti-oxidants and help to prevent inflammation and cancer. They also keep the teeth clean, are a great source of your daily dose of fiber, regulate the cholesterol level and prevent arthritis.',
+    SanitizerDesc :'Hand Sanitizer is an antiseptic which gives you 99.99% protection against disease-causing bacteria, germs and viruses.',
+    SugarDesc :'Sugar is finished from organic sugar cane, the grains are light cream colored. It is an improved option as opposed to refined sugar that is pure white in color.',
+    VegetableOilDesc :'Vegetable Oil is rich in a bio-active component, Oryzanol, that helps reduce cholesterol levels and has been researched and scientifically proven to reduce Cholesterol in 30 Days.',
 }
 
 export default ProductItemDesciption;
